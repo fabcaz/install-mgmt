afd02c06deb3da4c7266d87e962eb3e760078980
fabcaz <fcazeils1@protonmail.com>
2023-03-20 12:35:09 -0400

    initial commit

---
+++ src/main/java/com/example/installmgmt/InstallMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.example.installmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstallMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstallMgmtApplication.class, args);
+	}
+
+}

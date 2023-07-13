0dd0560e794f81da456bb1df647fc37c729651ad
fabcaz <fcazeils1@protonmail.com>
2023-07-13 19:37:10 -0400

    fixup faab4cc

--- src/main/java/com/example/installmgmt/dtos/AddressDto.java
+++ src/main/java/com/example/installmgmt/dtos/AddressDto.java
@@ -4,12 +4,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 @NoArgsConstructor
 public class AddressDto {
   

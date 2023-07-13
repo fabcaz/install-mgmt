03b31f335c29e7fe5a2c277ca8576d778dbbd840
fabcaz <fcazeils1@protonmail.com>
2023-07-13 19:39:26 -0400

    fixup 7d522cf

--- src/main/java/com/example/installmgmt/dtos/PersonDto.java
+++ src/main/java/com/example/installmgmt/dtos/PersonDto.java
@@ -3,12 +3,16 @@ package com.example.installmgmt.dtos;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 @NoArgsConstructor
 public class PersonDto{
   

24a2305519a973e666f61b9461f033205544198a
fabcaz <fcazeils1@protonmail.com>
2023-07-13 19:41:30 -0400

    fixup 006ad6c

--- src/main/java/com/example/installmgmt/dtos/InstallRequestAppointmentDto.java
+++ src/main/java/com/example/installmgmt/dtos/InstallRequestAppointmentDto.java
@@ -5,12 +5,16 @@ import java.time.LocalDateTime;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode
+@ToString
 @NoArgsConstructor
 public class InstallRequestAppointmentDto {
   
